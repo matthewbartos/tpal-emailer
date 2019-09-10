d2481a1eadd697b25577ad79bd7cd9fd0d83f58a
Mateusz Bartos <geekbajt@gmail.com>
2019-09-10 09:54:38 +0200

    Introducing JMenuBar

--- src/main/kotlin/com/matthewbartos/emailer/Main.kt
+++ src/main/kotlin/com/matthewbartos/emailer/Main.kt
@@ -2,29 +2,33 @@ package com.matthewbartos.emailer
 
 import java.awt.EventQueue
 import javax.swing.JFrame
+import javax.swing.JMenu
+import javax.swing.JMenuBar
+import javax.swing.JMenuItem
 
 class Main(title: String) : JFrame() {
 
     init {
-        createUI(title)
-    }
-
-    private fun createUI(title: String) {
-
         setTitle(title)
 
         defaultCloseOperation = EXIT_ON_CLOSE
         setSize(300, 200)
         setLocationRelativeTo(null)
+
+        jMenuBar = JMenuBar().apply {
+            add(
+                JMenu("Language").apply {
+                    add(JMenuItem("English"))
+                    add(JMenuItem("Polish"))
+                }
+            )
+        }
     }
 }
 
 private fun createAndShowGUI() {
-
-    val frame = Main("Simple")
+    val frame = Main("Mailer")
     frame.isVisible = true
 }
 
-fun main(args: Array<String>) {
-    EventQueue.invokeLater(::createAndShowGUI)
-}
+fun main() = EventQueue.invokeLater(::createAndShowGUI)
